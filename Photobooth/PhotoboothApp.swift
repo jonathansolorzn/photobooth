9c54ae90d228bde9036b455d9e5a9bb39bbf5f2b
Jonathan <fenixsolorzano@gmail.com>
2022-04-19 23:18:50 -0600

    Initial Commit

---
+++ Photobooth/PhotoboothApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoboothApp.swift
+//  Photobooth
+//
+//  Created by Jonathan Solorzano on 19/4/22.
+//
+
+import SwiftUI
+
+@main
+struct PhotoboothApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
